4ae6053fc0e7fb5cc6b3356f762d51b475d04436
Robert Rondon <robrondon11@gmail.com>
2022-02-13 16:59:46 -0300

    using Pic in Pic API and mediaStream API

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+const video = document.getElementById('video');
+const button = document.getElementById('button');
+
 // Prompt to select a media stream, pass to video element and play
+const selectMediaStream = async function () {
+  try {
+    const captureStream = await navigator.mediaDevices.getDisplayMedia();
+    video.srcObject = captureStream;
+    video.onloadedmetadata = () => {
+      video.play();
+    };
+  } catch (error) {
+    // Handle Error
+    console.log('Error:', error);
+  }
+};
+
+button.addEventListener('click', async () => {
+  try {
+    // Disable button
+    button.disabled = true;
+
+    // Start Picture in Picture
+    await video.requestPictureInPicture();
+
+    // Reset button
+    button.disabled = false;
+  } catch (error) {
+    // Handle Error
+    console.log('Error:', error);
+  }
+});
+
+// On Load
+selectMediaStream();
